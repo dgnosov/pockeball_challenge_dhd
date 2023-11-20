526f6b5423252789ab435a4e429623fa7b363191
Daniel <ccsnosovdg@gmail.com>
2023-11-20 22:35:47 +0300

    feat: add camera rotatetion with keyboard

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,58 @@ import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Scene from "./components/Scene";
 import Lights from "./components/Lights";
-import { Perf } from "r3f-perf";
+import { useEffect, useState } from "react";
+import { MOUSE } from "three/src/Three.js";
+import { useControls } from "leva";
 
 function App() {
+  const [leftMouse, setLeftMouse] = useState<undefined | MOUSE>(undefined);
+
+  const settings = useControls("CameraTest", {
+    camera: {
+      y: 5,
+      z: 5,
+      x: 0,
+    },
+  });
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.code === "AltLeft") {
+      setLeftMouse(MOUSE.LEFT);
+    }
+  };
+
+  const handleKeyUp = () => {
+    setLeftMouse(undefined);
+  };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener("keyup", handleKeyUp);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+      document.addEventListener("keyup", handleKeyUp);
+    };
+  }, []);
+
   return (
-    <Canvas camera={{ fov: 40 }} shadows={true}>
+    <Canvas
+      camera={{
+        fov: 14,
+        position: [settings.camera.x, settings.camera.y, settings.camera.z],
+      }}
+      shadows={true}
+    >
       {/* <Perf /> */}
       <Scene />
       <Lights />
-      <OrbitControls />
+      <OrbitControls
+        enableRotate
+        mouseButtons={{
+          LEFT: leftMouse,
+        }}
+      />
     </Canvas>
   );
 }
