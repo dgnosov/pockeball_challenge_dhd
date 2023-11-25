332d93bb55b3a616ab64d15b42a7c0ae1a9253a1
Daniel <ccsnosovdg@gmail.com>
2023-11-25 22:15:59 +0300

    feat: add loading screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-import { OrbitControls } from "@react-three/drei";
+import { Loader, OrbitControls } from "@react-three/drei";
 import "./App.css";
 import { Canvas } from "@react-three/fiber";
 import Scene from "./components/Scene";
 import Lights from "./components/Lights";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { MOUSE } from "three/src/Three.js";
 import { Perf } from "r3f-perf";
+import { LoadingScreen } from "./components/LoadingScreen/LoadingScreen";
 
 function App() {
   const [leftMouse, setLeftMouse] = useState<undefined | MOUSE>(undefined);
+  const [start, setStart] = useState(false);
 
   const handleKeyDown = (e: KeyboardEvent) => {
     if (e.code === "AltLeft") {
@@ -30,24 +32,34 @@ function App() {
     };
   }, []);
 
+  useEffect(() => {
+    if (start) {
+      // audio.play();
+    }
+  }, [start]);
+
   return (
-    <Canvas
-      camera={{
-        fov: 3,
-        position: [50, 0, 0],
-      }}
-      shadows={true}
-    >
-      {/* <Perf /> */}
-      <Scene />
-      <Lights />
-      <OrbitControls
-        enableRotate
-        mouseButtons={{
-          LEFT: leftMouse,
+    <>
+      <Canvas
+        camera={{
+          fov: 3,
+          position: [50, 0, 0],
         }}
-      />
-    </Canvas>
+        shadows={true}
+      >
+        {/* <Perf /> */}
+
+        <Scene />
+        <Lights />
+        <OrbitControls
+          enableRotate
+          mouseButtons={{
+            LEFT: leftMouse,
+          }}
+        />
+      </Canvas>
+      <LoadingScreen started={start} onStarted={() => setStart(true)} />
+    </>
   );
 }
 
